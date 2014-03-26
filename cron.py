7ad5335a3276316c88ae2604f8c2b4e39567cfd5
Gabe Fierro <gtfierro225@gmail.com>
2014-03-26 01:03:18 -0700

    try generating next timestep for deferLater

--- cron.py
+++ cron.py
@@ -1,4 +1,6 @@
 import re
+import datetime
+from itertools import cycle
 from twisted.internet import reactor, defer
 from twisted.internet.task import LoopingCall, deferLater
 
@@ -34,14 +36,100 @@ class CronSchedule(object):
         self._minute = self.generate('minute', minute)
         self._hour = self.generate('hour', hour)
         self._day_of_week = self.generate('day_of_week', day_of_week)
+        print "second",self._second
+        print "minute",self._minute
+        print "hour",self._hour
+        print "day_of_week",self._day_of_week
         if not (self._second or self._minute or self._hour or self._day_of_week):
             raise ValueError("CronSchedule must be instantiated with at least one timestep")
+        self.current = datetime.datetime.now()
 
     def generate(self, timeunit, cronstring):
-        if not cronstring: return []
+        if not cronstring: return None
         assert self._is_valid_chars(cronstring), "{0} contains invalid cron characters".format(cronstring)
         self._check_range(timeunit, cronstring)
         return self._parse_cronstring(timeunit, cronstring)
+
+    def _get_closest(self, value, values):
+        """
+        Given a value [value] and a list of values [values],
+        returns the value from [values] that is > [value]. If
+        no such values exist, returns the first item of [values].
+
+        Assumes [values] is sorted in ascending order
+        """
+        upper = filter(lambda x: x > value, values)
+        if len(upper):
+            return upper[0]
+        else:
+            return values[0]
+
+    def next(self):
+        """
+        Given the current time, generate the datetime object of the next calltime
+        according to the cron schedule.
+
+        *default to 0 instead of None on generate()?
+
+        - if there is a second schedule, get the next closest scheduled second, otherwise
+          the scheduled second is 0
+        - use above second as the second step. if the next second step is < scheduled, then 
+          we'll need to increment the minute
+        - get closest scheduled minute. if there is a second schedule, this is the current minute
+          unless we have the increment flag from above. If there is no second schedule, then this
+          should be the next minute step. If there is no minute schedule, this is 0. If the
+          next minute step is < the current scheduled step, then increment hour
+        - get closest scheduled hour. if there is a minute schedule, this is the current hour
+          unless we have the increment flag from above. If there is no minute schedule, then this
+          is the next hour step. If there is no hour schedule, then this is 0.
+        """
+        self.current = datetime.datetime.now()
+        nexttime = self.current
+        increment_minute = False
+        increment_hour = False
+        second = 0
+        minute = self.current.minute
+        hour = self.current.hour
+        print '-'*20
+        if self._second:
+            print 'have second schedule'
+            second = self._get_closest(self.current.second, self._second)
+            print 'next scheduled second:',second
+            next_second = self._get_closest(second, self._second)
+            print 'next next sched second:',next_second
+            if second == self._second[0]:
+                print 'increment minute!'
+                increment_minute = True
+
+        if self._minute:
+            print 'have minute schedule'
+            if self._second:
+                minute = self.current.minute + 1*increment_minute
+            else:
+                minute = self._get_closest(self.current.minute, self._minute)
+            print 'next sched min:', minute
+            next_minute = self._get_closest(minute, self._minute)
+            print 'next next sched min:',next_minute
+            if minute == self._minute[0]:
+                print 'increment hour!'
+                increment_hour = True
+
+        if self._hour:
+            if self._minute:
+                hour = self.current.hour + 1*increment_hour
+            else:
+                hour = self._get_closest(self.current.hour, self._hour)
+            next_hour = self._get_closest(hour, self._hour)
+            print 'hour',hour,next_hour
+            if hour == self._hour[0]:
+                increment_day = True
+
+        print 'Current:',self.current
+        nexttime = nexttime.replace(second=second, minute=minute, hour=hour, microsecond=0)
+        print "next call time:",nexttime
+        waittime = nexttime - self.current
+        print 'Wait',waittime.seconds
+        return waittime.seconds
 
     def _is_int(self, s):
         try:
